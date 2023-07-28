f7c221e62f9ec3eef615ab06a1454c1caf7a31f7
Ulrick12 <100319778+Ulrick12@users.noreply.github.com>
2023-07-28 15:57:31 -0600

    Add files via upload

---
+++ controllers/swalertas-norma.js
@@ -0,0 +1,62 @@
+Swal.fire({
+    title: 'Bienvenido',
+	//text:   '',
+	html: 'Puedes consultar la información y descargarla.<br><br> <label style="font-size: 13px"><b>Nota:</b> Verificar en la carpeta de descargas, si el archivo es descargado correctamente.</label>',
+	icon: 'info',
+	confirmButtonText: 'Aceptar',
+	// footer:
+	// width:
+	// padding:
+	// background:
+	// grow:
+	// backdrop:
+	// timer:
+	// timerProgressBar:
+	// toast:
+	position:'center',
+	// allowOutsideClick:
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	//  icon: 'info'
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+  	//cancelButtonText: "Cancelar",
+	//closeOnConfirm: true,
+	//closeOnCancel: false
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	// imageUrl:
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
